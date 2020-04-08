c08384f7235b1c33d01689b9cbb3fd6c941a9627
Viivi Puuronen <viivi.puuronen@hotmail.com>
2020-04-08 15:00:38 +0300

    korjauksia uuden artikkelin lomakkeeseen

--- application/artikkelit/forms.py
+++ application/artikkelit/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, IntegerField, BooleanField, validators
 class ArtikkeliForm(FlaskForm):
     name = StringField("Title", [validators.DataRequired()])
     publisher = StringField("Publisher", [validators.DataRequired()])
-    authors = StringField("Author", [validators.DataRequired()])
+    authors = StringField("Authors", [validators.DataRequired()])
     source = StringField("Source", [validators.DataRequired()])
     year = IntegerField("Year published", [validators.NumberRange(min=0, max=None, message="Positive integer required")])
     read = BooleanField("Read")
